7beb5e54d88069e25c62f2e1a4537d45a0e40706
alyaa999 <alyaamamoon999@gmail.com>
2025-04-24 18:45:34 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,63 @@
+﻿// Segregated interfaces
+public interface ITaskCreator
+{
+    void CreateSubTask();
+}
+
+public interface ITaskAssigner
+{
+    void AssignTask();
+}
+
+public interface ITaskWorker
+{
+    void WorkOnTask();
+}
+
+public class Task
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string AssignedTo { get; private set; }
+    public void AssignTo(Developer developer)
+    {
+        AssignedTo = developer.Name;
+        Console.WriteLine($"Task '{Title}' assigned to {AssignedTo}");
+    }
+}
+
+public class Developer
+{
+    public string Name { get; set; }    
+}
+
+public class TeamLead : ITaskCreator, ITaskAssigner, ITaskWorker
+{
+    public void AssignTask()
+    {
+        Task t = new Task()
+        {
+            Title = "Merge and Deploy",
+            Description = "Task to merge and deploy sharing feature to develop"
+        };
+        t.AssignTo(new Developer() { Name = "Developer1" });
+    }
+
+    public void CreateSubTask()
+    {
+        // Logic to create subtask
+    }
+
+    public void WorkOnTask()
+    {
+        // Logic to work on a task
+    }
+}
+
+public class Manager : ITaskAssigner
+{
+    public void AssignTask()
+    {
+        // Manager assigns a task but doesn’t work on it
+    }
+}
---
+++ SqlFile/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Text;
+
+public interface ITextLoader
+{
+    string LoadText();
+}
+
+public interface ITextSaver
+{
+    void SaveText();
+}
+
+public class SqlFile : ITextLoader, ITextSaver
+{
+    public string FilePath { get; set; }
+    public string FileText { get; set; }
+
+    public string LoadText()
+    {
+        return File.ReadAllText(FilePath);
+    }
+
+    public void SaveText()
+    {
+        File.WriteAllText(FilePath, FileText);
+    }
+}
+public class ReadOnlySqlFile : ITextLoader
+{
+    public string FilePath { get; set; }
+    public string FileText { get; set; }
+
+    public string LoadText()
+    {
+        return File.ReadAllText(FilePath);
+    }
+
+}
+
+public class SqlFileLoaderManager
+{
+    public List<ITextLoader> FilesToLoad { get; set; }
+
+    public string GetTextFromFiles()
+    {
+        var sb = new StringBuilder();
+        foreach (var file in FilesToLoad)
+        {
+            sb.Append(file.LoadText());
+        }
+        return sb.ToString();
+    }
+}
+
+public class SqlFileSaverManager
+{
+    public List<ITextSaver> FilesToSave { get; set; }
+
+    public void SaveTextIntoFiles()
+    {
+        foreach (var file in FilesToSave)
+        {
+            file.SaveText();
+        }
+    }
+}
